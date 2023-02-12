efcbf53d3ed6120c1d0882cd1eaabd445a10bcaf
Aiara Lobo Gomes <aiaragomes@protonmail.com>
2023-02-12 11:30:28 +0100

    Script to guide through root recovery process

---
+++ README.md
@@ -0,0 +1,83 @@
+# Recover root on LineageOS
+
+Script that guides you through the process of recovering root on a smartphone
+with *LineageOS* (LOS), rooted with *Magisk*, and with boot ramdisk. This is
+useful because root is lost after every LOS update. The script basically
+automates some of the steps of the 
+[Magisk installation guide](https://topjohnwu.github.io/Magisk/install.html),
+which includes getting the `boot.img`, and guides you through the steps that are
+more difficult to automate. You can also use this script if you are rooting your
+phone for the first time. Just make sure that you install the 
+[Magisk app](https://github.com/topjohnwu/Magisk/releases/tag/v25.2) before
+starting. Please read all steps before starting and *proceed at your own risk*.
+
+
+## Requirements
+
+- [required] Smartphone with LineageOS
+- [required] Magisk app installed
+- [required] Device's bootloader has to be unlocked
+- [required] Device with boot ramdisk
+- [required] A few basic Unix commands: `wget` & `sha256sum`
+- [required] Android tools: `adb` & `fastboot`
+- [required] Payload dumper (see next section)
+- [required] Python 3.6
+- [required] Pip
+- [optional] Pyenv
+- [optional] Virtualenv
+
+This script was tested under Linux.
+
+
+## Payload dumper setup
+
+The LOS boot image (`boot.img`) is needed to be able to root the phone with
+Magisk. You can extract it from the LOS installation package using 
+[payload dumper](https://github.com/vm03/payload_dumper). 
+You can run the following script to get payload dumper:
+
+``` bash
+  $ git clone https://github.com/aiaragomes/recover_phone_root.git
+  $ cd payload_dumper
+  $ chmod u+x install_payload_dumper.sh
+  $ ./install_payload_dumper.sh
+```
+
+
+## Usage
+
+### Adding script to your local commands
+
+You can add this script to you local commands to facilitate usage in the
+future.
+
+``` bash
+  $ chmod u+x recover_phone_root.sh
+  $ echo 'export PATH="$(pwd):$PATH"' >> $HOME/.bashrc  # add current dir to your PATH permanently
+  $ bash  # refresh terminal for change to take effect
+```
+
+### Using the script
+
+You can run the script to recover root as following:
+
+``` bash
+  $ recover_phone_root.sh <los_dir> <los_dumper>
+```
+
+where `<los_dir>` is the path where you want to store the LineageOS installation
+package and `<los_dumper>` is the path with the python script to extract the
+boot image from the LOS installation package. If you run the script from the
+root directory of this repo, you can for instance do the following:
+
+``` bash
+  $ mkdir lineageos
+  $ ./recover_phone_root.sh lineageos payload_dumper
+```
+
+The script will guide you through the necessary steps to recover root.
+You will be asked for some input while the script is running. You need to give
+the correct input to successfully complete the process. Please be mindful that 
+this script will flash a Magisk patched boot image, so proceed at your *own
+risk*. Have a good look at the script to see what it is doing before using it.
+
---
+++ recover_phone_root.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+# Recover root on android smartphone with LineageOS and Magisk
+# Usage: recover_phone_root.sh <los_dir> <los_dumper>
+
+# Downloading lineage os
+echo "-----> Downloading LineageOS"
+cd $1
+echo -n "Do you need to download the zip (y/n)? "
+read -n 1 choice
+echo ""
+if [[ $choice == "y" ]]; then
+  rm lineage*.zip lineage*sha256
+  echo -n "Date of the release (YYYYMMDD)? "
+  read -n 8 date
+  echo ""
+  wget https://mirrorbits.lineageos.org/full/FP3/$date/lineage-19.1-$date-nightly-FP3-signed.zip
+  wget https://mirrorbits.lineageos.org/full/FP3/$date/lineage-19.1-$date-nightly-FP3-signed.zip?sha256
+  echo ""
+  echo "Comparing checksum"
+  sha256sum -c lineage-19.1-$date-nightly-FP3-signed.zip?sha256
+fi
+echo ""
+
+# Getting boot.img
+echo "-----> Getting boot.img"
+cd $2
+echo -n "Do you need to get the boot.img (y/n)? "
+read -n 1 choice
+echo ""
+if [[ $choice == "y" ]]; then  
+  rm *.img
+  source .venv/bin/activate
+  cp $1/*.zip .
+  unzip lineage-19.1-$date-nightly-FP3-signed.zip
+  python payload_dumper.py payload.bin
+  deactivate
+  cp output/boot.img .
+  cp output/vbmeta.img .
+  rm lineage-19.1-$date-nightly-FP3-signed.zip
+  rm -rf META-INF/ __pycache__/ output/
+  rm apex_info.pb care_map.pb payload.bin payload_properties.txt
+fi
+echo ""
+
+# Checking adb
+echo "-----> Recovering root"
+echo "1) Please plug your phone via USB"
+echo ""
+echo -n "2) Is rooted USB debugging allowed on your phone (y/n)? "
+read -n 1 choice
+echo ""
+echo "You might need to give permission on your phone..."
+if [[ $choice == "y" ]]; then  
+  echo ""
+  echo "Checking adb..."
+  sudo adb kill-server
+  echo "Waiting 15 seconds..."
+  sleep 15
+  sudo adb start-server
+  adb devices
+else
+  echo ""
+  echo "It is not possible to continue..."
+  exit -1
+fi
+
+# Copying boot.img
+adb push boot.img /sdcard/Download/
+echo ""
+echo -n "3) Do you see boot.img in the Download folder (y/n)? "
+read -n 1 choice
+echo ""
+if [[ $choice == "n" ]]; then  
+  echo "There might be a problem with adb..."
+  exit -1
+fi
+echo ""
+
+# Recovering root
+echo "4) Reinstall/update the Magisk app from the apk or older version"
+echo -n "Have you done it (y/n)? "
+read -n 1 choice
+echo ""
+if [[ $choice == "n" ]]; then  
+  echo "You need Magisk to continue..."
+  exit -1
+fi
+echo ""
+
+echo "5) Please go to the Magisk app and patch the new boot image"
+echo -n "Have you patched it (y/n)? "
+read -n 1 choice
+echo ""
+if [[ $choice == "y" ]]; then  
+        echo -n "What is the code of the patched image (NNNNN_LLLLL)? "
+  read -n 11 code
+  echo ""
+  adb pull /sdcard/Download/magisk_patched-$code.img magisk_patched.img
+  adb reboot fastboot
+else
+  echo "It is not possible to continue..."
+  exit -1
+fi
+echo ""
+
+echo -n "6) Are you in fastboot mode (y/n)? "
+read -n 1 choice
+echo ""
+if [[ $choice == "y" ]]; then  
+  sudo fastboot flash boot magisk_patched.img
+else
+  echo "It is not possible to continue..."
+  exit -1
+fi
+echo ""
+
+echo -n "7) Do you want to flash vbmeta (y/n)? "
+read -n 1 choice
+echo ""
+if [[ $choice == "y" ]]; then  
+  sudo fastboot flash vbmeta --disable-verity --disable-verification vbmeta.img
+fi
+
+echo ""
+echo "-----> Final steps"
+echo "You can restart your phone now!"
+echo ""
+echo "Recommended final steps:"
+echo "- disable USB debugging"
+echo "- hide the Magisk app"
+echo "- uninstall Magisk on Work space / Shelter (if you have it)"
+echo "- clean up Download dir"
+echo ""
+echo "Done! Enjoy your rooted phone! ;D"
+echo ""
+
